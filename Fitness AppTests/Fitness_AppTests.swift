feca946c80329ea027cf4c495b62c145590bf9c8
Amir <bagasharov10@gmail.com>
2025-12-07 14:56:02 +0800

    Initial Commit

---
+++ Fitness AppTests/Fitness_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  Fitness_AppTests.swift
+//  Fitness AppTests
+//
+//  Created by Amir on 07.12.2025.
+//
+
+import Testing
+@testable import Fitness_App
+
+struct Fitness_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
